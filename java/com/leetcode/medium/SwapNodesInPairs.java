7d896aec362c551ab2d0a234a6279ae858488173
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2022-07-11 00:10:20 +0530

    Add SwapNodesInPairs

---
+++ java/com/leetcode/medium/SwapNodesInPairs.java
@@ -0,0 +1,20 @@
+package com.leetcode.medium;
+
+import com.leetcode.utils.ListNode;
+
+public class SwapNodesInPairs {
+
+	public ListNode swapPairs(ListNode head) {
+	    rec(head);
+	    return head;
+    }
+    
+    public void rec (ListNode head) {
+	    if (head != null && head.next != null) {
+	        int oddVal = head.val;
+	        head.val = head.next.val;
+	        head.next.val = oddVal;
+	        rec(head.next.next);
+        }
+    }
+}
