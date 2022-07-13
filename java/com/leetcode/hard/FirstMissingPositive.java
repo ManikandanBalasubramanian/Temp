7c8d538840a2c4632e52cba9278c117fe2ef6ba2
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2022-07-13 11:23:56 +0530

    Add FirstMissingPositive

---
+++ java/com/leetcode/hard/FirstMissingPositive.java
@@ -0,0 +1,22 @@
+package com.leetcode.hard;
+
+public class FirstMissingPositive {
+
+    public int firstMissingPositive(int[] nums) {
+  	    boolean myArr [] = new boolean[300];
+	    int index = 1;
+	    for(int i = 0; i < nums.length ; i++) {
+	        if(nums[i] <= 300 && nums[i] > 0) {
+		        myArr[nums[i]-1] = true; 
+	        }
+	    }
+
+	    for(int i = 0; i < 300; i++ ) {
+	        if(!myArr[i]) {
+		        index = i+1;
+		        break;
+	        }
+	    }
+	    return index;
+    }
+}
