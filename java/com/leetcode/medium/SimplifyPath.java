912c9b7a6902a662f9020e4307890a6c493bd5b4
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2022-07-14 19:26:15 +0530

    Add SimplifyPath

---
+++ java/com/leetcode/medium/SimplifyPath.java
@@ -0,0 +1,43 @@
+package com.leetcode.medium;
+
+import java.util.List;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+import java.util.*;
+
+
+public class SimplifyPath {
+
+   public String simplifyPath(String path) {
+
+        path = path.replaceAll("/\\./","/");
+        path = path.replaceAll("//","/");
+        
+        List<String> paths = Arrays.stream(path.split("/")).collect(Collectors.toList());
+        
+        for(int i = 0; i < paths.size() ; ) {
+            
+            if(paths.get(i).equals("")){
+                paths.remove(i);
+                
+            } else if(paths.get(i).equals("..")) {
+                paths.remove(i);            
+                if(i > 0) {
+                    paths.remove(i-1);
+                    i--;
+                }
+            } else if(paths.get(i).equals(".")){
+                paths.remove(i);
+            } else {
+                i++;
+            }
+        }
+        
+        path = paths.stream().filter(s-> !s.equals("")).collect(Collectors.joining("/"));
+                
+        return "/" + path;
+    }
+
+
+
+}
