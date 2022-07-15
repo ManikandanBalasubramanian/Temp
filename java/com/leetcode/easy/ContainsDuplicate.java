4e171fee898bca95828334c989f17190e47dcd03
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2022-07-15 20:29:56 +0530

    Add ContainsDuplicate

---
+++ java/com/leetcode/easy/ContainsDuplicate.java
@@ -0,0 +1,20 @@
+package com.leetcode.easy;
+
+import java.util.Set;
+import java.util.HashSet;
+
+public class ContainsDuplicate {
+
+	public boolean containsDuplicate(int[] nums) {
+        
+        Set<Integer> set = new HashSet<>();
+        
+        for(int num : nums) {
+            if(set.contains(num)) {
+                return true;
+            }
+            set.add(num);
+        }
+        return false;
+    }
+}
