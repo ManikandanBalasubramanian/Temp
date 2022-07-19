a38edf84eeffeac374b7b4487714db28c5fa9012
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2022-07-19 16:53:27 +0530

    Start LeetCode75 Add RunningSum1dArray

---
+++ leetcode75/com/leetcode/easy/RunningSum1dArray.java
@@ -0,0 +1,12 @@
+package com.leetcode.easy;
+
+public class RunningSum1dArray {
+
+    public int[] runningSum(int[] nums) {                
+        for(int i = 1; i< nums.length; i++) {
+            nums[i] = nums[i] + nums[i-1];
+        }
+        return nums;
+    }
+
+}
