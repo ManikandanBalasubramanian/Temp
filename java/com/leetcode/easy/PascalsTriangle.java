14c18936be12b234dff9fe2f12a3c5184b977291
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2022-07-19 15:59:29 +0530

    Add Pascal's Triangle

---
+++ java/com/leetcode/easy/PascalsTriangle.java
@@ -0,0 +1,29 @@
+package com.leetcode.easy;
+
+import java.util.List;
+import java.util.ArrayList;
+
+
+public class PascalsTriangle {
+
+	public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> output = new ArrayList<>();
+        
+        for( int i=0 ; i< numRows ; i++) {
+            List<Integer> list = new ArrayList<>();
+            
+            for(int j = 0; j<=i;j++){
+                
+                if(j==0 || j==i) {
+                    list.add(1);    
+                } else {
+                    list.add(output.get(i-1).get(j) + output.get(i-1).get(j-1)); 
+                }    
+            }
+            
+            output.add(list);
+        }
+        
+        return output;
+    }
+}
