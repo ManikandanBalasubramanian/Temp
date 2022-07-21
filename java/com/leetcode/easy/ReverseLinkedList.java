40e23ad725236a07fb7f6d8106997f5e167cf94c
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2022-07-21 14:56:13 +0530

    Move all the Solutions under Java and Add ReverseLinkedList

---
+++ java/com/leetcode/easy/ReverseLinkedList.java
@@ -0,0 +1,18 @@
+package com.leetcode.easy;
+
+import com.leetcode.utils.ListNode;
+
+public class ReverseLinkedList {
+	public ListNode reverseList(ListNode head) {
+        ListNode prev = null, next = null;
+        
+        while(head != null) {
+            next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+        
+        return prev;
+    }
+}
