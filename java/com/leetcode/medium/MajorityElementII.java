dd2b5dea250fae6ce52642c4b0f455a60fd068ca
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2022-07-29 00:11:37 +0530

    Add MajorityElementII

---
+++ java/com/leetcode/medium/MajorityElementII.java
@@ -0,0 +1,32 @@
+package com.leetcode.medium;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+
+public class MajorityElementII {
+
+    public List<Integer> majorityElement(int[] nums) {
+        Map<Integer,Integer> map = new HashMap<>();
+        List<Integer> list = new ArrayList<>();
+        
+        for(int i : nums) {
+            if(map.containsKey(i)) {
+                map.put(i, map.get(i)+1);
+            } else {
+                map.put(i,1);
+            }
+        }
+        
+        int thres = (int) nums.length/3;
+        
+        for(int i : map.keySet()) {
+            if(map.get(i) > thres) {
+                list.add(i);
+            }
+        }
+        
+        return list;
+    }
+}
