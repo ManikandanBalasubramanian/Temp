2a1319a4cbbccae6e1693aca2740e05c11d683a3
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2022-08-04 18:47:00 +0530

    Update RemoveElement from O(n2) to O(n)

--- java/com/leetcode/easy/RemoveElement.java
+++ java/com/leetcode/easy/RemoveElement.java
@@ -3,19 +3,21 @@ package com.leetcode.easy;
 public class RemoveElement {
 
   public int removeElement(int[] nums, int val) {
-    int size = nums.length;
+    int n = nums.length;
+    int swapPointer = -1;
+    int index = 0;
 
-    for (int i = nums.length - 1; i >= 0; i--) {
-
-      if (val == nums[i]) {
-        for (int j = i; j < size - 1; j++) {
-          int temp = nums[j];
-          nums[j] = nums[j + 1];
-          nums[j + 1] = temp;
-        }
-        size--;
+    for (int i = 0; i < n; i++) {
+      if (swapPointer == -1 && nums[i] == val) {
+        swapPointer = i;
+        index++;
+      } else if (swapPointer != -1 && nums[i] != val) {
+        nums[swapPointer++] = nums[i];
+      } else if (nums[i] == val) {
+        index++;
       }
     }
-    return size;
+
+    return n - index;
   }
 }
