9aae7fa170fba71e54ba98f9a9f09431fbde43a4
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2022-08-16 22:03:00 +0530

    Add JumpGameII

---
+++ java/com/leetcode/medium/JumpGameII.java
@@ -0,0 +1,35 @@
+package com.leetcode.medium;
+
+public class JumpGameII {
+
+    public int jump(int[] nums) {
+        int[] bArr = new int[nums.length];
+        int n = nums.length;
+        int counter = 0;
+        
+        for(int i = 0; i < n; i++) {
+            int index = (nums[i] + i) >= n ? n-1 : (nums[i] + i);
+            populate(bArr, i, index);  
+        }
+                
+        
+        while(--n > 0) {
+            n = bArr[n]; 
+            counter++;
+        }
+        
+
+        return counter;
+    }
+    
+    public void populate(int[] bArr, int start, int end) {
+        int i = start + 1;
+        while (start <= end) {
+            
+            if(bArr[start] == 0) {
+                bArr[start] = i;
+            }
+            start++;
+        }
+    }
+}
