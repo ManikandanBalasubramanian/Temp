ed8a471008450ec73d1bec22da0edd09d2b637c2
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2022-08-17 21:39:05 +0530

    Add Trapping RainWater

---
+++ java/com/leetcode/hard/TrappingRainWater.java
@@ -0,0 +1,36 @@
+package com.leetcode.hard;
+
+public class TrappingRainWater {
+
+    public int trap(int[] height) {
+        int total = 0;
+        int start = 0, prev = 0;
+        
+        for(int i = 0; i < height.length; i++) {
+
+            if(height[i] > prev) total +=  calculate(height, start, i);
+            
+            if(height[i] >= height[start]) start = i;
+        
+            prev = height[i];
+        }
+     
+        return total;
+    }
+    
+    public int calculate(int[] height, int start, int end) {
+        int min = Math.min(height[start], height[end]);
+        int total = 0;
+        
+        while(start < end) {
+            if(height[start] < min) {
+                total += (min - height[start]);
+                height[start] = min;                
+            }
+            start++;
+        }
+        
+        
+        return total;
+    }
+}
