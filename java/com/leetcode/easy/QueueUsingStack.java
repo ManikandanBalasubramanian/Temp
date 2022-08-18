94495ff42e37952d3af621e2a6b1fd166ec8e5b9
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2022-08-18 15:20:35 +0530

    Add QueueUsingStack

---
+++ java/com/leetcode/easy/QueueUsingStack.java
@@ -0,0 +1,34 @@
+package com.leetcode.easy;
+
+import java.util.Stack;
+
+public class QueueUsingStack {
+
+    Stack<Integer> s = new Stack<>();
+    
+    public void push(int x) {
+        Stack<Integer> temp = new Stack<>();
+            
+        while(!s.empty()) {
+            temp.push(s.pop());
+        }
+        
+        temp.push(x);        
+        
+        while(!temp.empty()) {
+            s.push(temp.pop());
+        }
+    }
+    
+    public int pop() {
+        return s.pop();
+    }
+    
+    public int peek() {
+        return s.peek();
+    }
+    
+    public boolean empty() {
+        return s.empty();
+    }
+}
