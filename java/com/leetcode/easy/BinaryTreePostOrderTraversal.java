b1eda3b0abe0bd17d80d8ab808bda40e63cbcbb0
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2022-08-20 00:23:40 +0530

    Add BinaryTreePostOrderTraversal

---
+++ java/com/leetcode/easy/BinaryTreePostOrderTraversal.java
@@ -0,0 +1,18 @@
+package com.leetcode.easy;
+
+import com.leetcode.utils.TreeNode;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BinaryTreePostOrderTraversal {
+
+    List<Integer> list = new ArrayList<>();
+    
+    public List<Integer> postorderTraversal(TreeNode root) {
+        if(root == null) return list;
+        postorderTraversal(root.left);
+        postorderTraversal(root.right);
+        list.add(root.val);
+        return list;
+    }
+}
