6bc3d829d9b43d617bbe2db5a585245953194815
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2022-08-21 14:24:20 +0530

    Add PeakIndexInaMountainArray

---
+++ java/com/leetcode/medium/PeakIndexInaMountainArray.java
@@ -0,0 +1,22 @@
+package com.leetcode.medium;
+
+public class PeakIndexInaMountainArray {
+
+    public int peakIndexInMountainArray(int[] arr) {
+        int start = 0, end = arr.length -1;
+        
+        while(start <= end) {
+            int mid = start + (end - start)/2;
+            
+            if(arr[mid] > arr[mid-1] && arr[mid] > arr[mid+1]) {
+                return mid;
+            } else if(arr[mid] < arr[mid-1]) {
+                end = mid;
+            } else {
+                start = mid;
+            }
+        }
+        
+        return start;
+    }
+}
