b273ab89a80ccb2c555848fc5578f98cc6138299
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2022-09-17 20:36:47 +0530

    Add CombinationSum

---
+++ java/com/leetcode/medium/CombinationSum.java
@@ -0,0 +1,37 @@
+package com.leetcode.medium;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class CombinationSum {
+
+    List<List<Integer>> resultList = new ArrayList<>();
+    
+    public List<List<Integer>> combinationSum(int[] arr, int target) {
+            
+        getTargetCombination(arr, 0, target, new ArrayList<Integer>());
+        return resultList;
+    }
+    
+    
+    public void getTargetCombination(int[] arr, int position, int currentTarget, List<Integer> result) {
+
+        if (currentTarget == 0) {
+            resultList.add(new ArrayList<>(result));
+            return;
+        }
+        if (position == arr.length) {
+            return;
+        }
+
+        if (arr[position] <= currentTarget) {
+            result.add(arr[position]);
+            getTargetCombination(arr, position, currentTarget - arr[position], result);
+
+            result.remove(result.size() - 1);
+        }
+
+        getTargetCombination(arr, position + 1, currentTarget, result);
+    }
+    
+}
