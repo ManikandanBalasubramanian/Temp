8da8bee2fa6a6b3636976478ffa14cfcb6171eab
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2022-09-20 22:25:03 +0530

    Add LongestPalindromicSubstring

--- java/com/leetcode/medium/LongestPalindromicSubstring.java
+++ java/com/leetcode/medium/LongestPalindromicSubstring.java
@@ -2,48 +2,28 @@ package com.leetcode.medium;
 
 public class LongestPalindromicSubstring {
 
+private int lo, maxLen;
+
 	public String longestPalindrome(String s) {
-	    int windowStart = 0, windowEnd = 1;
-        int length = s.length();
-	    int maxLength = 0;
-	    String palindrome = "";
-
-	    while(windowStart < length) {            
-	        if(isPalindrome(s.substring(windowStart, windowEnd))) {
-		        if (maxLength < windowEnd-windowStart) {
-		            maxLength = windowEnd-windowStart;
-		            palindrome = s.substring(windowStart, windowEnd);
-		        }    
-	        }
-	        windowEnd++;
-
-	        if(maxLength == length) {
-		        return palindrome;
-	        }
-	    
-	        if((windowStart + maxLength) >= length) {
-		        return palindrome;
-	        }
-            
-	        if(windowEnd > length) {
-		        windowStart++;
-		        windowEnd = windowStart+1;
-	        }
+		int len = s.length();
+		
+	    if (len < 2) return s;
+		
+	    for (int i = 0; i < len-1; i++) {
+	     	extendPalindrome(s, i, i);  
+	     	extendPalindrome(s, i, i+1);
 	    }
+	    return s.substring(lo, lo + maxLen);
+	}
 
-	    return palindrome;
-    }
-    
-    
-    
-    private boolean isPalindrome(String s) {
-		int half = s.length()/2;
-		for(int i = 0; i < half; i++)
-		{
-		    if(s.charAt(i) != s.charAt(s.length()-i-1)) {
-				return false;
-		    }
+	private void extendPalindrome(String s, int j, int k) {
+		while (j >= 0 && k < s.length() && s.charAt(j) == s.charAt(k)) {
+			j--;
+			k++;
 		}
-		return true;
-    }
+		if (maxLen < k - j - 1) {
+			lo = j + 1;
+			maxLen = k - j - 1;
+		}
+	}
 }
