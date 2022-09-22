eeddc70d1dfb5a8131741806383ceeff9ff8a3b1
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2022-09-22 22:41:07 +0530

    Add SpiralMatrixII

---
+++ java/com/leetcode/medium/SpiralMatrixII.java
@@ -0,0 +1,43 @@
+package com.leetcode.medium;
+
+public class SpiralMatrixII {
+
+
+    int val = 0;
+    int loop = 0;
+    
+    public int[][] generateMatrix(int n) {
+        int[][] output = new int[n][n];           
+        right(0, n-1, 0, output);
+                
+        return output;
+    }
+    
+    public void right(int start, int end, int i, int[][] mat) {
+        if(start > end) return;
+        while(start <= end) mat[i][start++] = ++val;
+        down(i+1, mat.length-loop-1, --start, mat);
+    }
+
+    public void down(int start, int end, int j, int[][] mat) {
+        if(start > end) return;
+        while(start <= end) mat[start++][j] = ++val;
+        left(j-1, loop, --start, mat);
+
+    }
+
+    public void left(int start, int end, int i, int[][] mat) {
+        if(start < end) return;
+        while(start >= end) mat[i][start--] = ++val;
+        up(i-1, ++loop, ++start, mat);
+    }
+
+    
+    public void up(int start, int end, int j, int[][] mat) {
+        if(start < end) return;
+        while(start >= end) mat[start--][j] = ++val;
+        right(j+1, mat.length-loop-1, ++start, mat);
+    }
+        
+    
+}
