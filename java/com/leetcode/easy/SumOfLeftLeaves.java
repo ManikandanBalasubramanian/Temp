51fedf4ea54f0e9655b104372d7c2ebe001557e4
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2022-09-24 21:09:25 +0530

    Add SumOfLeftLeaves

---
+++ java/com/leetcode/easy/SumOfLeftLeaves.java
@@ -0,0 +1,19 @@
+package com.leetcode.easy;
+
+import com.leetcode.utils.TreeNode;
+
+public class SumOfLeftLeaves {
+
+    public int sumOfLeftLeaves(TreeNode root) {
+       return sumLeft(root, false);
+    }
+    
+    public int sumLeft(TreeNode root, boolean isLeft) {
+        if(root == null) return 0;
+        
+        if(root.left == null && root.right == null && isLeft) return root.val;
+
+        return sumLeft(root.left, true) + sumLeft(root.right, false);
+
+    }
+}
