a852a0217c3604798ed69dd8cad2247d00d22976
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2022-09-27 22:00:15 +0530

    After PrisonCellsAfterNDays

---
+++ java/com/leetcode/medium/PrisonCellsAfterNDays.java
@@ -0,0 +1,19 @@
+package com.leetcode.medium;
+
+public class PrisonCellsAfterNDays {
+    public int[] prisonAfterNDays(int[] cells, int N) {
+        N = N % 14 == 0 ? 14 : N % 14;
+        for(int i = 0; i < N; i++) {
+            cells = next(cells);
+        }
+        return cells;
+    }
+    
+    private int[] next(int[] cells) {
+        int[] next = new int[cells.length];
+        for (int i = 1; i < cells.length - 1; i++) {
+            next[i] = (cells[i - 1] == cells[i + 1]) ? 1 : 0;
+        }
+        return next;
+    }
+}
