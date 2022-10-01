18232ef2b719a0bd9c548f8250a2cd509c9782d0
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2022-10-01 19:15:31 +0530

    Add KthSymbolInGrammar

---
+++ java/com/leetcode/medium/KthSymbolInGrammar.java
@@ -0,0 +1,15 @@
+package com.leetcode.medium;
+
+public class KthSymbolInGrammar {
+	
+    public int kthGrammar(int N, int K) {
+        if(N == 1 && K == 1) return 0;
+
+        int prevLayerSize = (int) Math.pow(2, N - 2);
+        if(K <= prevLayerSize) {
+            return kthGrammar(N - 1, K);
+        } else {
+            return kthGrammar(N - 1, K - prevLayerSize) ^ 1;
+        }
+    }
+}
