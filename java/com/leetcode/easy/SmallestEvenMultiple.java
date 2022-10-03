c32fafb2d98b56c8f2c2c868a5e6237b0942648a
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2022-10-04 00:12:12 +0530

    Update SmallestEvenMultiple

--- java/com/leetcode/easy/SmallestEvenMultiple.java
+++ java/com/leetcode/easy/SmallestEvenMultiple.java
@@ -2,6 +2,6 @@ package com.leetcode.easy;
 
 public class SmallestEvenMultiple {
 	public int smallestEvenMultiple(int n) {
-        return (n&1) == 1 ? n*2 : n;
+        return n*(n&1) + n;
     }
 }
