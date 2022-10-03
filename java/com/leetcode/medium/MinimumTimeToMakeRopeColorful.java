a5e01858cd685d4b4467fd8a85c1c9d1e7b67973
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2022-10-03 22:14:51 +0530

    Add MinimumTimeToMakeRopeColorful

---
+++ java/com/leetcode/medium/MinimumTimeToMakeRopeColorful.java
@@ -0,0 +1,20 @@
+package com.leetcode.medium;
+
+public class MinimumTimeToMakeRopeColorful {
+
+    public int minCost(String colors, int[] neededTime) {
+        int totalTime = 0, currMaxTime = 0;
+        
+        for (int i = 0; i < colors.length(); ++i) {
+
+            if (i > 0 && colors.charAt(i) != colors.charAt(i - 1)) {
+                currMaxTime = 0;
+            }
+            
+            totalTime += Math.min(currMaxTime, neededTime[i]);
+            currMaxTime = Math.max(currMaxTime, neededTime[i]);
+        }
+        
+        return totalTime;
+    }
+}
