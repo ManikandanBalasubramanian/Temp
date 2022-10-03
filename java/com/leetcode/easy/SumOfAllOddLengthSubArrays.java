bd8a23e9c86e3643ecb3297efec583dafc32fa5c
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2022-10-04 00:11:54 +0530

    Add SumOfAllOddLengthSubArrays

---
+++ java/com/leetcode/easy/SumOfAllOddLengthSubArrays.java
@@ -0,0 +1,14 @@
+package com.leetcode.easy;
+
+public class SumOfAllOddLengthSubArrays {
+
+    public int sumOddLengthSubarrays(int[] arr) { 
+        int res = 0;
+        for (int i = 0; i < arr.length; i++) {
+            int allCount = (arr.length - i) * (i + 1);
+            int oddCount = allCount % 2 == 0 ? allCount / 2 : allCount / 2 + 1;
+            res += oddCount * arr[i];
+        }
+        return res;
+    }
+}
