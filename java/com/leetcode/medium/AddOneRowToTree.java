c87a3dcd5d275c76d1af9cc31ccaf5eca464d319
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2022-10-05 11:59:12 +0530

    Add OneRowToTree

---
+++ java/com/leetcode/medium/AddOneRowToTree.java
@@ -0,0 +1,28 @@
+package com.leetcode.medium;
+
+import com.leetcode.utils.TreeNode;
+
+public class AddOneRowToTree {
+
+    boolean isLeft = true;
+    
+    public TreeNode addOneRow(TreeNode root, int val, int depth) {
+        if(depth == 1) {
+            TreeNode node = new TreeNode(val);
+            if(isLeft) node.left = root;
+            else node.right = root;
+            
+            return node;
+        }
+        
+        if(root == null) return null;
+        
+        isLeft = true;
+        root.left = addOneRow(root.left, val, depth -1);
+        
+        isLeft = false;
+        root.right = addOneRow(root.right, val, depth -1);
+        
+        return root;
+    }
+}
