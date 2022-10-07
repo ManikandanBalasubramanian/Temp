fcd3432ca7e316c76dddb4b6076f7786a99939d9
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2022-10-07 23:30:00 +0530

    Add SortTheSentence

---
+++ java/com/leetcode/easy/SortTheSentence.java
@@ -0,0 +1,12 @@
+package com.leetcode.easy;
+
+public class SortTheSentence {
+
+    public String sortSentence(String s) {
+        String []ans = new String[s.split(" ").length];
+        for(String st: s.split(" ")){
+            ans[st.charAt(st.length()-1) - '1'] = st.substring(0,st.length()-1);
+        }
+        return String.join(" ", ans);
+    }
+}
