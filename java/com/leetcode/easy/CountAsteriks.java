02edc46683d4857c15e4de642d3b0120c3486ff7
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2022-10-09 22:16:45 +0530

    Add CountAsteriks

---
+++ java/com/leetcode/easy/CountAsteriks.java
@@ -0,0 +1,17 @@
+package com.leetcode.easy;
+
+public class CountAsteriks {
+
+    public int countAsterisks(String s) {
+        int asteriks = 0;
+        int lines = 0;
+        for (char c: s.toCharArray()){
+            switch (c) {
+            case '*': if (lines%2==0)asteriks++; break;
+            case '|': lines++; break;
+            default: break;
+            }
+        }
+        return asteriks;
+    }
+}
