31cccf879695fe88d2988584f6c12a221e0ab559
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2022-10-13 22:36:56 +0530

    Add ShuffleTheArray

---
+++ java/com/leetcode/easy/ShuffleTheArray.java
@@ -0,0 +1,19 @@
+package com.leetcode.easy;
+
+public class ShuffleTheArray {
+    public int[] shuffle(int[] nums, int n) {
+        int max = 1001;
+        for(int i=n;i<nums.length;i++){
+            nums[i] = max*nums[i]+nums[i-n];
+        }
+        
+        int ind = 0;
+        for(int i=n;i<nums.length;i++){
+            nums[ind] = nums[i]%max;
+            nums[ind+1] = nums[i]/max;
+            ind+=2;
+        }
+        
+        return nums;
+    }
+}
