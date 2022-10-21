0815263bbc9e95bf798f2f2e7561f4f856ceeb03
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2022-10-21 14:52:53 +0530

    Add ContainsDuplicateII

---
+++ java/com/leetcode/easy/ContainsDuplicateII.java
@@ -0,0 +1,15 @@
+package com.leetcode.easy;
+
+import java.util.Set;
+import java.util.HashSet;
+
+public class ContainsDuplicateII {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<Integer>();
+        for(int i = 0; i < nums.length; i++){
+            if(i > k) set.remove(nums[i-k-1]);
+            if(!set.add(nums[i])) return true;
+        }
+        return false;
+    }
+}
