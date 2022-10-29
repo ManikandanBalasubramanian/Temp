2e6b76cb867bd23444982e5afc3b953e21c39e1a
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2022-10-29 11:01:16 +0530

    Add EarliestPossibleDayOfFullBloom

---
+++ java/com/leetcode/hard/EarliestPossibleDayOfFullBloom.java
@@ -0,0 +1,26 @@
+package com.leetcode.hard;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
+
+public class EarliestPossibleDayOfFullBloom {
+
+    public int earliestFullBloom(int[] plantTime, int[] growTime) {
+        int n = growTime.length;
+        List<Integer> indices = new ArrayList<>();
+        for (int i = 0; i < n; ++i) {
+            indices.add(i);
+        }
+        Collections.sort(indices, Comparator.comparingInt(i -> -growTime[i]));
+        int result = 0;
+        for (int i = 0, curPlantTime = 0; i < n; ++i) {
+            int idx = indices.get(i);
+            int time = curPlantTime + plantTime[idx] + growTime[idx];
+            result = Math.max(result, time);
+            curPlantTime += plantTime[idx];
+        }
+        return result;
+    }
+}
