fe7598a2713daf43c7e2037b4ad42c2c05d380cf
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2022-11-05 09:35:25 +0530

    Add GenerateParantheses

---
+++ java/com/leetcode/medium/GenerateParantheses.java
@@ -0,0 +1,19 @@
+package com.leetcode.medium;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class GenerateParantheses {
+    public List<String> generateParenthesis(int n) {
+        List<String> ans = new ArrayList();
+        if (n == 0) {
+            ans.add("");
+        } else {
+            for (int c = 0; c < n; ++c)
+                for (String left: generateParenthesis(c))
+                    for (String right: generateParenthesis(n-1-c))
+                        ans.add("(" + left + ")" + right);
+        }
+        return ans;
+    }
+}
