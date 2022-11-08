aee8c42a7e37554a87e8539a086feb3c21906ca2
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2022-11-08 22:02:35 +0530

    Add MakeTheStringGreat

---
+++ java/com/leetcode/easy/MakeTheStringGreat.java
@@ -0,0 +1,16 @@
+package com.leetcode.easy;
+
+public class MakeTheStringGreat {
+
+    public String makeGood(String s) {
+        int n = s.length();
+        
+        for (int i = 0; i < n - 1; i++) {
+            if (Math.abs(s.charAt(i) - s.charAt(i+1)) == 32) {
+                return makeGood(s.substring(0, i) + s.substring(i+2));
+            }
+        }
+        
+        return s;
+    }
+}
