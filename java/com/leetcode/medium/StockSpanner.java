5ead6e3f03fb27ce37c4eb2de30a53289e31ebc5
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2022-11-09 22:54:11 +0530

    Add StockSpanner

---
+++ java/com/leetcode/medium/StockSpanner.java
@@ -0,0 +1,22 @@
+package com.leetcode.medium;
+
+import java.util.Stack;
+
+public class StockSpanner {
+
+    Stack<int[]> stack = new Stack<>();
+    
+    public StockSpanner() {
+        
+    }
+    
+    public int next(int price) {
+        int ans = 1;
+        while (!stack.isEmpty() && stack.peek()[0] <= price) {
+            ans += stack.pop()[1];
+        }
+        
+        stack.push(new int[] {price, ans});
+        return ans;
+    }
+}
