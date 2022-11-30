ec6ab603070707221ef2907663c4ce327a69d6e0
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2022-12-01 02:21:57 +0530

    Add UniqueNumberOfOccurrences

---
+++ java/com/leetcode/easy/UniqueNumberOfOccurrences.java
@@ -0,0 +1,23 @@
+package com.leetcode.easy;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.HashSet;
+
+public class UniqueNumberOfOccurrences {
+
+    public boolean uniqueOccurrences(int[] arr) {
+        Map<Integer, Integer> count = new HashMap<>();
+        for (int a : arr) {
+            count.merge(a, 1, Integer::sum);
+        }
+        Set<Integer> seen = new HashSet<>();
+        for (int freq : count.values()) {
+            if (!seen.add(freq)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
