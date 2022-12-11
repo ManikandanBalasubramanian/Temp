fbb41b10e0d91a908a7c365104fa318516a93a0c
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2022-12-11 22:43:31 +0530

    Add BinaryTreeMaximumPathSum

---
+++ java/com/leetcode/hard/BinaryTreeMaximumPathSum.java
@@ -0,0 +1,24 @@
+package com.leetcode.hard;
+
+import com.leetcode.util.TreeNode;
+
+public class BinaryTreeMaximumPathSum {
+
+    public int maxPathSum(TreeNode root) {
+        maxSum = Integer.MIN_VALUE;
+        gainFromSubtree(root);
+        return maxSum;
+    }
+
+    private int maxSum;
+    private int gainFromSubtree(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int gainFromLeft = Math.max(gainFromSubtree(root.left), 0);
+        int gainFromRight = Math.max(gainFromSubtree(root.right), 0);
+        maxSum = Math.max(maxSum, gainFromLeft + gainFromRight + root.val);
+        return Math.max(gainFromLeft + root.val, gainFromRight + root.val);
+    }
+}
