ea98e9a19339e813c42dfa4f8be56f4a11b2dd64
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2022-12-12 21:43:08 +0530

    Add LongestSquareStreakInArray

---
+++ java/com/leetcode/medium/LongestSquareStreakInArray.java
@@ -0,0 +1,23 @@
+package com.leetcode.medium;
+
+import java.util.Set;
+import java.util.HashSet;
+
+public class LongestSquareStreakInArray {
+
+    public int longestSquareStreak(int[] nums) {
+        int max = 0;
+        Set<Integer> set = new HashSet<>();
+        for(int i: nums) set.add(i);
+        for(int i=0;i<nums.length;i++) {
+            int num = nums[i];
+            int count = 1;
+            while(set.contains(num*num)) {
+                num = num*num;
+                count++;
+            }
+            if(count > 1 && count > max) max = count;
+        }
+        return max==0 ? -1 : max;
+    }
+}
