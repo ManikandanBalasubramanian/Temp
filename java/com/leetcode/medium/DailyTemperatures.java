a6055a1ffba14153bf2f986f1bc8c5281d0f925d
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2022-12-18 18:31:05 +0530

    Add Daily Temperatures

---
+++ java/com/leetcode/medium/DailyTemperatures.java
@@ -0,0 +1,26 @@
+package com.leetcode.medium;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Stack;
+
+public class DailyTemperatures {
+
+    public int[] dailyTemperatures(int[] temperatures) {
+    	
+        final int m = temperatures.length;
+        final Map<Integer, Integer> next = new HashMap<>();
+        final Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < m; i++) {
+            while (!stack.empty() && temperatures[stack.peek()] < temperatures[i]) {
+                next.put(stack.peek(), i - stack.pop());
+            }
+            stack.push(i);
+        }
+        final int[] ans = new int[m];
+        for (int i = 0; i < m; i++) {
+            ans[i] = next.getOrDefault(i, 0);
+        }
+        return ans;
+    }	
+}
