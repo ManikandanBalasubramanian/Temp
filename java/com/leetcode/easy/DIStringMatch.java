aacb4604cba8eeff940a50ed17de76b3074cde9d
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2022-12-26 22:15:15 +0530

    Add DIStringMatch

---
+++ java/com/leetcode/easy/DIStringMatch.java
@@ -0,0 +1,19 @@
+package com.leetcode.easy;
+
+public class DIStringMatch {
+
+    public int[] diStringMatch(String S) {
+        int N = S.length();
+        int lo = 0, hi = N;
+        int[] ans = new int[N + 1];
+        for (int i = 0; i < N; ++i) {
+            if (S.charAt(i) == 'I')
+                ans[i] = lo++;
+            else
+                ans[i] = hi--;
+        }
+
+        ans[N] = lo;
+        return ans;
+    }
+}
