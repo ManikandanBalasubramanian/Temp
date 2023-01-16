5279ec08e72cf7836edc341e38b22725475e1b33
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2023-01-16 22:45:17 +0530

    Add InsertInterval

---
+++ java/com/leetcode/medium/InsertInterval.java
@@ -0,0 +1,28 @@
+package com.leetcode.medium;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+
+public class InsertInterval {
+
+    public int[][] insert(int[][] intervals, int[] newInterval) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> (a[0] - b[0]));
+        for (int[] interval : intervals) pq.add(interval);
+        pq.add(newInterval);
+        
+        List<int[]> res = new ArrayList<>(); 
+        int[] prev = pq.poll();        
+        while (!pq.isEmpty()) {             
+            int[] cur = pq.poll();
+            if (prev[1] >= cur[0]) {
+                prev = new int[]{prev[0], Math.max(cur[1], prev[1])};
+            } else {
+                res.add(prev);
+                prev = cur;
+            }
+        }
+        res.add(prev);       
+        return res.toArray(new int[res.size()][]);
+    }
+}
