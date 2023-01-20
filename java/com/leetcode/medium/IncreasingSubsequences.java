caa51e03e6600604a865be61c4de7f2a9b2b6cca
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2023-01-20 22:08:32 +0530

    Add Increasing Subsequences

---
+++ java/com/leetcode/medium/IncreasingSubsequences.java
@@ -0,0 +1,29 @@
+package com.leetcode.medium;
+
+import java.util.List;
+import java.util.Set;
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class IncreasingSubsequences {
+
+    public List<List<Integer>> findSubsequences(int[] nums) {
+        Set<List<Integer>>al=new HashSet<List<Integer>>();
+        List<Integer>ds=new ArrayList<>();
+        check(al,nums,0,ds,Integer.MIN_VALUE);
+        List res=new ArrayList(al);
+        return res;
+    }
+    public static void check(Set<List<Integer>>al,int nums[],int ind,List<Integer>ds,int prev){
+        if(ds.size()>1){
+            al.add(new ArrayList<>(ds));
+        }
+        for(int i=ind;i<nums.length;i++){
+            if(nums[i]>=prev){
+                ds.add(nums[i]);
+                check(al,nums,i+1,ds,nums[i]);
+                ds.remove(ds.size()-1);
+            }
+        }
+    }
+}
