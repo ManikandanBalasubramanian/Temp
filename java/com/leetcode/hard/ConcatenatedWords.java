f648e4a4e9e29c40aa6bcce8b6fcb3da4ed4035f
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2023-01-27 22:21:41 +0530

    Add ConcatenatedWords

---
+++ java/com/leetcode/hard/ConcatenatedWords.java
@@ -0,0 +1,35 @@
+package com.leetcode.hard;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.HashSet;
+
+public class ConcatenatedWords {
+
+  public List<String> findAllConcatenatedWordsInADict(String[] words) {
+        List<String> al = new ArrayList<>();
+        Set<String> hs = new HashSet<>();
+        for(String i:words) hs.add(i);
+        Map<String,Integer> hm = new HashMap<>();
+        for(String i:words) if(check(i,hs,hm)) al.add(i);
+        return al;
+    }
+
+  public static boolean check(String i, Set<String> hs, Map<String,Integer> hm) {
+        if(hm.containsKey(i)) return hm.get(i) == 1;
+        for(int z = 0; z < i.length(); z++) {
+            if(hs.contains(i.substring(0,z))) {
+                String s1 = i.substring(z);
+                if(hs.contains(s1)||check(s1,hs,hm)) {
+                    hm.put(i,1);
+                    return true;
+                }
+            }
+        }
+        hm.put(i,0);
+        return false;
+    }
+}
