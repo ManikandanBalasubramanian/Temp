598967dbc0626435ab2e83034d86d0a39b65dd26
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2023-02-16 10:27:11 +0530

    Update MaximumDepthOfBinaryTree

--- java/com/leetcode/easy/MaximumDepthOfBinaryTree.java
+++ java/com/leetcode/easy/MaximumDepthOfBinaryTree.java
@@ -3,7 +3,7 @@ package com.leetcode.easy;
 import com.leetcode.utils.TreeNode;
 
 public class MaximumDepthOfBinaryTree {
-  
+
     public int maxDepth(TreeNode root) {
         if(root == null) return 0;
         return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
