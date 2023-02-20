4d5abd4cbbd651be38eb81b65b4f8f1644d920e6
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2023-02-20 09:46:07 +0530

    Update SearchInsertPosition

--- java/com/leetcode/easy/SearchInsertPosition.java
+++ java/com/leetcode/easy/SearchInsertPosition.java
@@ -3,6 +3,7 @@ package com.leetcode.easy;
 public class SearchInsertPosition {
 
 	public int searchInsert(int[] nums, int target) {
+        
         int i = 0;
         for( ; i < nums.length; i++) {
         	if(nums[i] >= target) {
