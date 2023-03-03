c4440786307b8d739a896f5b150ba10380134084
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2023-03-03 08:31:00 +0530

    Add IndexOfFirstOccurrenceInString

---
+++ java/com/leetcode/medium/IndexOfFirstOccurrenceInString.java
@@ -0,0 +1,28 @@
+package com.leetcode.medium;
+
+
+public class IndexOfFirstOccurrenceInString {
+
+  public int strStr(String haystack, String needle) {
+        
+    int nSize = needle.length();
+    int hSize = haystack.length();
+
+    for (int i = 0; i < hSize; i++) {
+      int pointer = 0;
+      int startIndex = -1;
+
+      if (haystack.charAt(i) == needle.charAt(pointer) && (hSize - i) >= (nSize)) {
+        startIndex = i;
+        int j =i;
+        while (pointer++ < nSize - 1 && haystack.charAt(++j) == needle.charAt(pointer)) {}
+      }
+
+      if (pointer == nSize) {
+        return startIndex;
+      }
+    }
+
+    return -1;
+  }
+}
