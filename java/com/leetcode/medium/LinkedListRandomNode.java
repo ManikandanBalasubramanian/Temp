2edbb03f2b0d1448c98916a09fd40a0636e9aaae
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2023-03-11 10:45:59 +0530

    Add LinkedListRandomNode

---
+++ java/com/leetcode/medium/LinkedListRandomNode.java
@@ -0,0 +1,19 @@
+package com.leetcode.medium;
+
+import java.util.ArrayList;
+
+public class LinkedListRandomNode {
+
+    ArrayList<Integer>list = new ArrayList<>();
+    public Solution(ListNode head) {
+        while(head != null){
+            list.add(head.val);
+            head = head.next;
+        }
+    }
+    
+    public int getRandom() {
+        double random = Math.random()*list.size();
+        return list.get((int)random);
+    }
+}
