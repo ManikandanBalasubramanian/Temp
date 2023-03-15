34782b43643b9878246e5642d511ccba84c63f59
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2023-03-15 23:29:35 +0530

    Add CompletenessOfaBinaryTree

---
+++ java/com/leetcode/medium/CompletenessOfaBinaryTree.java
@@ -0,0 +1,34 @@
+package com.leetcode.medium;
+
+import com.leetcode.utils.TreeNode;
+
+public class CompletenessOfaBinaryTree {
+
+    public boolean isCompleteTree(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        boolean seenNull = false;
+        
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            
+            if (node == null) {
+                seenNull = true;
+                continue;
+            }
+            
+            if (seenNull) {
+                return false;
+            }
+            
+            queue.add(node.left);
+            queue.add(node.right);
+        }
+        
+        return true;
+    }
+}
