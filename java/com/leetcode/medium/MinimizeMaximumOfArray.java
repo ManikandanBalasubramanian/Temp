1ff05802ededbb7649b5bc7f5ca3bee895587988
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2023-04-04 05:18:49 +0530

    Add MinimizeMaximumOfArray

---
+++ java/com/leetcode/medium/MinimizeMaximumOfArray.java
@@ -0,0 +1,12 @@
+package com.leetcode.medium;
+
+public class MinimizeMaximumOfArray {
+
+    public int minimizeArrayValue(int[] nums) {
+        long sum = 0, max = 0;
+		for (int i = 0; i < nums.length; i++) {
+			max = Math.max(max, ((sum += nums[i]) + i) / (i + 1));
+		}
+		return (int) max;
+    }
+}
