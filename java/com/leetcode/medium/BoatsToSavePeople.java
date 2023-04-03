0447221385be9ecff1262681e46e0cd015dc8eb4
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2023-04-03 20:12:48 +0530

    Add BoatsToSavePeople

---
+++ java/com/leetcode/medium/BoatsToSavePeople.java
@@ -0,0 +1,18 @@
+package com.leetcode.medium;
+
+public class BoatsToSavePeople {
+
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int i = 0, j = people.length - 1;
+        int boats = 0;
+        while (i <= j) {
+            if (people[j] + people[i] <= limit) {
+                i++;
+            }
+            j--;
+            boats++;
+        }
+        return boats;
+    }
+}
