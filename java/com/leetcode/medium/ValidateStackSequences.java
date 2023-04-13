871bef079e2a00717d5d645a78802b0b72a336e7
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2023-04-13 09:26:24 +0530

    Add ValidateStackSequences

---
+++ java/com/leetcode/medium/ValidateStackSequences.java
@@ -0,0 +1,21 @@
+package com.leetcode.medium;
+
+public class ValidateStackSequences {
+
+   public boolean validateStackSequences(int[] pushed, int[] poped) {
+
+     int k=0,j=0;
+    
+      for( int x :pushed) {
+           pushed[k++] = x ;
+            
+           while( k!=0 && poped[j]== pushed[k-1] ) {
+                j++;
+                k--; 
+            }
+          
+      }
+    
+      return k==0;
+    }
+}
