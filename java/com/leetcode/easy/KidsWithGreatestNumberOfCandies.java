ad287a55b5d3c882737a6ac0a5cb40afea223b23
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2023-04-17 10:27:47 +0530

    Add KidsWithGreatestNumberOfCandies

---
+++ java/com/leetcode/easy/KidsWithGreatestNumberOfCandies.java
@@ -0,0 +1,23 @@
+package com.leetcode.easy;
+
+public class KidsWithGreatestNumberOfCandies {
+    
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int maxCandies = 0;
+        for (int candy : candies) {
+            maxCandies = Math.max(maxCandies, candy);
+        }
+        
+        List<Boolean> result = new ArrayList<>();
+        
+        for (int candy : candies) {
+            if (candy + extraCandies >= maxCandies) {
+                result.add(true);
+            } else {
+                result.add(false);
+            }
+        }
+        
+        return result;
+    }
+}
