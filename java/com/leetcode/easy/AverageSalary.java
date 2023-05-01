c9ab522467917c10bd4571b99790f142165dbfbb
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2023-05-01 07:22:25 +0530

    Add AverageSalary

---
+++ java/com/leetcode/easy/AverageSalary.java
@@ -0,0 +1,28 @@
+package com.leetcode.easy;
+
+
+public class AverageSalary {
+
+    public double average(int[] salary) {
+        int minSalary = Integer.MAX_VALUE;
+        int maxSalary = Integer.MIN_VALUE;
+        int totalSalary = 0;
+        int numEmployees = 0;
+        
+        for (int s : salary) {
+            if (s < minSalary) {
+                minSalary = s;
+            }
+            if (s > maxSalary) {
+                maxSalary = s;
+            }
+            totalSalary += s;
+            numEmployees++;
+        }
+        
+        totalSalary -= minSalary + maxSalary;
+        numEmployees -= 2;
+        
+        return (double) totalSalary / numEmployees;
+    }
+}
