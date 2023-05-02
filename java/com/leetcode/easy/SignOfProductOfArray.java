d8746f60b3235b1447463db62f80f062a36cc0b3
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2023-05-02 11:00:52 +0530

    Add SignOfProductOfArray

---
+++ java/com/leetcode/easy/SignOfProductOfArray.java
@@ -0,0 +1,23 @@
+package com.leetcode.easy;
+
+public class SignOfProductOfArray {
+
+    public int arraySign(int[] nums) {
+        int n=nums.length;
+        int prod = 1;
+        for(int i = 0; i < n; i++){
+            if(nums[i] > 0) {
+                prod *= 1 ;
+            } else if(nums[i]<0){
+                prod *= -1;
+            } else prod *= 0;
+        }
+        return signFunc(prod);
+    } 
+
+    public int signFunc(long product){
+        if(product<0) return -1;
+        else if(product>0) return 1;
+        else return 0;
+    }
+}
