c939a8fb6b82a8c736899254a33014a792c22c40
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2023-05-04 11:42:59 +0530

    Add Dota2Senate

---
+++ java/com/leetcode/medium/Dota2Senate.java
@@ -0,0 +1,35 @@
+package com.leetcode.medium;
+
+
+public class Dota2Senate {
+
+    public String predictPartyVictory(String senate) {
+        
+        int rCount = 0, dCount = 0;
+        boolean[] vote = new boolean[senate.length()];
+        
+        while (rCount < senate.length() && dCount < senate.length()) {
+            for (int i = 0; i < senate.length() && rCount < senate.length() && dCount < senate.length(); i++) {
+
+                if (vote[i])
+                    continue;
+                
+                if (senate.charAt(i) == 'R') {
+                    if (rCount < dCount)
+                        vote[i] = true;
+                    rCount++;    
+                } 
+                else {
+                    if (dCount < rCount)
+                        vote[i] = true;
+                    dCount++;
+                }
+            }
+        }
+        
+        if (rCount == senate.length())
+            return "Radiant";
+        else
+            return "Dire";
+    }
+}
