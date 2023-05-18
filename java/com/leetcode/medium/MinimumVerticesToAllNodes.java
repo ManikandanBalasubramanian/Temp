a522d7148f100c874c1b1fd44411eed7c13ad408
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2023-05-18 21:23:16 +0530

    Add MinimumVerticesToAllNodes

---
+++ java/com/leetcode/medium/MinimumVerticesToAllNodes.java
@@ -0,0 +1,22 @@
+package com.leetcode.medium;
+
+public class MinimumVerticesToAllNodes {
+
+    public List<Integer> findSmallestSetOfVertices(int n, List<List<Integer>> edges) {
+        Set<Integer> destinations = new HashSet<>();
+        
+        for (List<Integer> edge : edges) {
+            destinations.add(edge.get(1));
+        }
+        
+        List<Integer> result = new ArrayList<>();
+        
+        for (int i = 0; i < n; i++) {
+            if (!destinations.contains(i)) {
+                result.add(i);
+            }
+        }
+        
+        return result;
+    }
+}
