d74d1631d0157dfc38201c506de3e2d735adf471
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2023-05-29 09:54:22 +0530

    Update ParkingSystem

--- java/com/leetcode/easy/ParkingSystem.java
+++ java/com/leetcode/easy/ParkingSystem.java
@@ -1,18 +1,21 @@
 package com.leetcode.easy;
 
 public class ParkingSystem {
-    int a[] ;
+
+    int a[];
+
     public ParkingSystem(int big, int medium, int small) {
         a = new int[3];
         a[0] = big;
         a[1] = medium;
         a[2] = small;
     }
-    
+
     public boolean addCar(int carType) {
-        if (a[carType - 1] <= 0) return false;
+        if (a[carType - 1] <= 0)
+            return false;
         a[carType - 1]--;
         return true;
-      
+
     }
 }
