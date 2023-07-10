4f95e7e31d8a2f1ce79fc578af1dccb1210a66fe
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2023-07-10 11:21:58 +0530

    Add MinimumDepthOfBinaryTree

---
+++ java/com/leetcode/easy/MinimumDepthOfBinaryTree.java
@@ -0,0 +1,12 @@
+package com.leetcode.easy;
+
+public class MinimumDepthOfBinaryTree {
+
+    public int minDepth(TreeNode root) {
+        if (root == null)
+            return 0;
+        int L = minDepth(root.left), R = minDepth(root.right);
+        return 1 + (Math.min(L, R) > 0 ? Math.min(L, R) : Math.max(L, R));
+    }
+
+}
