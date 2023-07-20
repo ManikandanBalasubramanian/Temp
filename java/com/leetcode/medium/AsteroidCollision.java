d8926af186fe5759389ae747f650ba42a480d7aa
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2023-07-20 10:52:22 +0530

    Add AsteroidCollision

---
+++ java/com/leetcode/medium/AsteroidCollision.java
@@ -0,0 +1,21 @@
+package com.leetcode.medium;
+
+public class AsteroidCollision {
+
+    public int[] asteroidCollision(int[] a) {
+        LinkedList<Integer> s = new LinkedList<>();
+        for (int i : a) {
+            if (i > 0)
+                s.add(i);
+            else {
+                while (!s.isEmpty() && s.getLast() > 0 && s.getLast() < -i)
+                    s.pollLast();
+                if (!s.isEmpty() && s.getLast() == -i)
+                    s.pollLast();
+                else if (s.isEmpty() || s.getLast() < 0)
+                    s.add(i);
+            }
+        }
+        return s.stream().mapToInt(i -> i).toArray();
+    }
+}
