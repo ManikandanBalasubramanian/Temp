297b7d41a29dd02bf021e6b3271b86826136f3bd
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2023-08-08 12:00:24 +0530

    Update SearchA2DMatrix

--- java/com/leetcode/medium/SearchA2DMatrix.java
+++ java/com/leetcode/medium/SearchA2DMatrix.java
@@ -5,23 +5,22 @@ public class SearchA2DMatrix {
     public boolean searchMatrix(int[][] matrix, int target) {
         int m = matrix.length;
         int n = matrix[0].length;
-        
-        int start = 0, end = (m*n) -1;
-        
-        while(start <= end) {
-            int mid = start + (end - start)/2;
+
+        int start = 0, end = (m * n) - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
             int r = mid / n;
             int c = mid % n;
-            
-            if(matrix[r][c] == target) {
+
+            if (matrix[r][c] == target) {
                 return true;
-            } else if(matrix[r][c] > target) {
+            } else if (matrix[r][c] > target) {
                 end = mid - 1;
             } else {
                 start = mid + 1;
             }
         }
-        
         return false;
     }
 }
