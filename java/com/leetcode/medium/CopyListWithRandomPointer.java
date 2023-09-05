46a85effca7ab811a83f779e6ffcf20c66961c08
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2023-09-05 10:50:22 +0530

    Update CopyListWithRandomPointer

--- java/com/leetcode/medium/CopyListWithRandomPointer.java
+++ java/com/leetcode/medium/CopyListWithRandomPointer.java
@@ -4,26 +4,25 @@ import com.leetcode.utils.Node;
 import java.util.List;
 import java.util.ArrayList;
 
-
 public class CopyListWithRandomPointer {
 
-	public Node copyRandomList(Node head) {
+    public Node copyRandomList(Node head) {
         List<Node> nodeList = new ArrayList<>();
         List<Node> nodeList2 = new ArrayList<>();
-        
-        while(head != null) {
+
+        while (head != null) {
             Node node;
-            
-            if(!nodeList.contains(head)) {
+
+            if (!nodeList.contains(head)) {
                 node = new Node(head.val);
                 nodeList.add(head);
                 nodeList2.add(node);
             } else {
                 node = nodeList2.get(nodeList.indexOf(head));
             }
-            
-            if(head.next != null) {
-                if(nodeList.contains(head.next)) {
+
+            if (head.next != null) {
+                if (nodeList.contains(head.next)) {
                     node.next = nodeList2.get(nodeList.indexOf(head.next));
 
                 } else {
@@ -33,9 +32,8 @@ public class CopyListWithRandomPointer {
                 }
             }
 
-                        
-            if(head.random != null) {
-                if(nodeList.contains(head.random)) {
+            if (head.random != null) {
+                if (nodeList.contains(head.random)) {
                     node.random = nodeList2.get(nodeList.indexOf(head.random));
 
                 } else {
@@ -46,8 +44,7 @@ public class CopyListWithRandomPointer {
             }
             head = head.next;
         }
-        
+
         return nodeList.size() > 0 ? nodeList2.get(0) : null;
     }
-
 }
