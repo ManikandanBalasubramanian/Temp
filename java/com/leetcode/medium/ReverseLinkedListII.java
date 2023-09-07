84698d56481b37802d20f71335b91bab0e4a0ef8
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2023-09-07 11:47:47 +0530

    Add ReverseLinkedListII

--- java/com/leetcode/medium/ReverseLinkedListII.java
+++ java/com/leetcode/medium/ReverseLinkedListII.java
@@ -3,49 +3,50 @@ package com.leetcode.medium;
 import com.leetcode.utils.ListNode;
 
 public class ReverseLinkedListII {
-    public ListNode reverseBetween(ListNode head, int left, int right) {       
+
+    public ListNode reverseBetween(ListNode head, int left, int right) {
         int index = 1;
         ListNode leftStart = null, start = null, temp = head, prev = null, next = null;
-                  
-        if(left == right) return head;
-        
-        while(index <= right +1) {
-            
-            if(index == left-1) {
+
+        if (left == right)
+            return head;
+
+        while (index <= right + 1) {
+
+            if (index == left - 1) {
 
                 leftStart = temp;
-                
-            } else if(index == right + 1) {
-                
+
+            } else if (index == right + 1) {
+
                 start.next = temp;
                 break;
-                
-            } else if(index == left) {
+
+            } else if (index == left) {
 
                 prev = temp;
                 start = temp;
-                
-            } else if(index == right) {
+
+            } else if (index == right) {
 
                 if (leftStart == null) {
                     head = temp;
-                } else { 
+                } else {
                     leftStart.next = temp;
                 }
-                
-            } 
-            if(index >left && index <= right) {
+
+            }
+            if (index > left && index <= right) {
 
                 next = temp.next;
                 temp.next = prev;
                 prev = temp;
                 temp = next;
-                
+
             } else {
                 temp = temp.next;
             }
-                
-                
+
             index++;
         }
         return head;
