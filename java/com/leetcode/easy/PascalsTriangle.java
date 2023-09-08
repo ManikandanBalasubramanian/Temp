e24abe31d455e7d2fca9e1f9a5b7810bb991e90a
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2023-09-08 11:48:33 +0530

    Update PascalsTriangle

--- java/com/leetcode/easy/PascalsTriangle.java
+++ java/com/leetcode/easy/PascalsTriangle.java
@@ -3,27 +3,26 @@ package com.leetcode.easy;
 import java.util.List;
 import java.util.ArrayList;
 
-
 public class PascalsTriangle {
 
-	public List<List<Integer>> generate(int numRows) {
+    public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> output = new ArrayList<>();
-        
-        for( int i=0 ; i< numRows ; i++) {
+
+        for (int i = 0; i < numRows; i++) {
             List<Integer> list = new ArrayList<>();
-            
-            for(int j = 0; j<=i;j++){
-                
-                if(j==0 || j==i) {
-                    list.add(1);    
+
+            for (int j = 0; j <= i; j++) {
+
+                if (j == 0 || j == i) {
+                    list.add(1);
                 } else {
-                    list.add(output.get(i-1).get(j) + output.get(i-1).get(j-1)); 
-                }    
+                    list.add(output.get(i - 1).get(j) + output.get(i - 1).get(j - 1));
+                }
             }
-            
+
             output.add(list);
         }
-        
+
         return output;
     }
 }
