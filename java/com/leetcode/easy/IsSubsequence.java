aede09fc8f58d37001e508391c31b685d01c7478
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2023-09-22 10:28:35 +0530

    Update IsSubsequence

--- java/com/leetcode/easy/IsSubsequence.java
+++ java/com/leetcode/easy/IsSubsequence.java
@@ -1,20 +1,21 @@
 package com.leetcode.easy;
 
 public class IsSubsequence {
+
     public boolean isSubsequence(String s, String t) {
-        
+
         int sLen = s.length();
         int count = 0;
         int i = 0;
-        
-        for(String ts : t.split("")) {
-            
-            if(i < sLen && ts.equals(s.charAt(i)+"" )) {
+
+        for (String ts : t.split("")) {
+
+            if (i < sLen && ts.equals(s.charAt(i) + "")) {
                 count++;
                 i++;
             }
         }
-        
-        return count == sLen;           
+
+        return count == sLen;
     }
 }
