eac006d80a4bb10ad3537a4d8cadfcfe3ace85ac
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2023-10-05 08:19:20 +0530

    Update MajorityElement

--- java/com/leetcode/medium/MajorityElementII.java
+++ java/com/leetcode/medium/MajorityElementII.java
@@ -8,25 +8,24 @@ import java.util.HashMap;
 public class MajorityElementII {
 
     public List<Integer> majorityElement(int[] nums) {
-        Map<Integer,Integer> map = new HashMap<>();
+        Map<Integer, Integer> map = new HashMap<>();
         List<Integer> list = new ArrayList<>();
-        
-        for(int i : nums) {
-            if(map.containsKey(i)) {
-                map.put(i, map.get(i)+1);
+
+        for (int i : nums) {
+            if (map.containsKey(i)) {
+                map.put(i, map.get(i) + 1);
             } else {
-                map.put(i,1);
+                map.put(i, 1);
             }
         }
-        
-        int thres = (int) nums.length/3;
-        
-        for(int i : map.keySet()) {
-            if(map.get(i) > thres) {
+
+        int thres = (int) nums.length / 3;
+
+        for (int i : map.keySet()) {
+            if (map.get(i) > thres) {
                 list.add(i);
             }
         }
-        
         return list;
     }
 }
