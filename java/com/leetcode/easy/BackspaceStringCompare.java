19673086517ae9612f026c2de2f5831eae595459
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2023-10-19 21:30:00 +0530

    Update BackspaceStringCompare

--- java/com/leetcode/easy/BackspaceStringCompare.java
+++ java/com/leetcode/easy/BackspaceStringCompare.java
@@ -9,28 +9,27 @@ public class BackspaceStringCompare {
         int tBackspace = 0;
         StringBuilder sBuilder = new StringBuilder();
         StringBuilder tBuilder = new StringBuilder();
-        
-        for(int i = s.length()-1; i >= 0; i--) {
-            if(s.charAt(i) != '#' && sBackspace == 0) {
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (s.charAt(i) != '#' && sBackspace == 0) {
                 sBuilder.append(s.charAt(i));
-            } else if(s.charAt(i) != '#' && sBackspace > 0) {
+            } else if (s.charAt(i) != '#' && sBackspace > 0) {
                 sBackspace--;
             } else {
                 sBackspace++;
             }
         }
-        
-         for(int i = t.length()-1; i >= 0; i--) {
-            if(t.charAt(i) != '#' && tBackspace == 0) {
+
+        for (int i = t.length() - 1; i >= 0; i--) {
+            if (t.charAt(i) != '#' && tBackspace == 0) {
                 tBuilder.append(t.charAt(i));
-            } else if(t.charAt(i) != '#' && tBackspace > 0) {
+            } else if (t.charAt(i) != '#' && tBackspace > 0) {
                 tBackspace--;
             } else {
                 tBackspace++;
             }
         }
-        
+
         return sBuilder.toString().equals(tBuilder.toString());
-        
     }
 }
