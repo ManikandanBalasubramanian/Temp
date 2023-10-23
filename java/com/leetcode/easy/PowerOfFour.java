023586f35fa7e8c2b52fb7cfcc492edfbd9f6483
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2023-10-23 16:40:43 +0530

    Update PowerOfFour

--- java/com/leetcode/easy/PowerOfFour.java
+++ java/com/leetcode/easy/PowerOfFour.java
@@ -1,6 +1,7 @@
 package com.leetcode.easy;
 
 public class PowerOfFour {
+    
     public boolean isPowerOfFour(int n) {
         double val = Math.log(n) /Math.log(4);
         return val == (double)(int)val;
