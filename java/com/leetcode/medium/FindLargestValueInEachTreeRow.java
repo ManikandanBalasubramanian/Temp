e79c3948ff9c8d14160a213a23fa7646bd803096
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2023-10-24 16:49:13 +0530

    Update FindLargestValueInEachTreeRow

--- java/com/leetcode/medium/FindLargestValueInEachTreeRow.java
+++ java/com/leetcode/medium/FindLargestValueInEachTreeRow.java
@@ -8,23 +8,25 @@ import java.util.ArrayList;
 public class FindLargestValueInEachTreeRow {
 
     List<Integer> list = new ArrayList<>();
-    
+
     public List<Integer> largestValues(TreeNode root) {
         solve(root, 0);
         return list;
     }
-    
+
     public void solve(TreeNode root, int level) {
-        if(root == null) return;
-        
-        if(list.size() <= level) {
+        if (root == null)
+            return;
+
+        if (list.size() <= level) {
             list.add(root.val);
         } else {
-            int max = Math.max(list.get(level) , root.val);
+            int max = Math.max(list.get(level), root.val);
             list.set(level, max);
         }
-        
+
         solve(root.left, level + 1);
         solve(root.right, level + 1);
     }
+
 }
