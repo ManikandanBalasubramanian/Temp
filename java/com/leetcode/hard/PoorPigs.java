c5db701a22356f3f2d5c688a6963ca7954a4b8ee
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2023-10-29 22:00:04 +0530

    Update PoorPigs

--- java/com/leetcode/hard/PoorPigs.java
+++ java/com/leetcode/hard/PoorPigs.java
@@ -3,14 +3,15 @@ package com.leetcode.hard;
 public class PoorPigs {
 
     public int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
-        int time = (int)Math.floor(minutesToTest/minutesToDie) + 1;
-        int out = (int)Math.floor(Math.log(buckets)/Math.log(time));
-        
-        double x = Math.log(buckets)/Math.log(time);
-        x = (x*1000000);
-        x = x/1000000;
-        if (x == (int)x) return out;
-        
+        int time = (int) Math.floor(minutesToTest / minutesToDie) + 1;
+        int out = (int) Math.floor(Math.log(buckets) / Math.log(time));
+
+        double x = Math.log(buckets) / Math.log(time);
+        x = (x * 1000000);
+        x = x / 1000000;
+        if (x == (int) x)
+            return out;
+
         return out + 1;
     }
 }
