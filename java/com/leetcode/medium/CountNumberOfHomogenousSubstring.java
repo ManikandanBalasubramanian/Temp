57cbb1ed8de67950cee99068840b19e3e64ae65d
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2023-11-08 23:05:40 +0530

    Add CountNumberOfHomogenousSubstring

---
+++ java/com/leetcode/medium/CountNumberOfHomogenousSubstring.java
@@ -0,0 +1,14 @@
+package com.leetcode.medium;
+
+public class CountNumberOfHomogenousSubstring {
+
+    public int countHomogenous(String s) {
+        int res = 0, cur = 0, count = 0, mod = 1_000_000_007;
+        for (int i = 0; i < s.length(); ++i) {
+            count = s.charAt(i) == cur ? count + 1 : 1;
+            cur = s.charAt(i);
+            res = (res + count) % mod;
+        }
+        return res;
+    }
+}
