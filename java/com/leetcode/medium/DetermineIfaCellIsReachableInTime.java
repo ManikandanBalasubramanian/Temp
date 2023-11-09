08ed3318c387eba22cbc9ef15f4972806723a57a
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2023-11-09 23:03:33 +0530

    Add DetermineIfaCellIsReachableInTime

---
+++ java/com/leetcode/medium/DetermineIfaCellIsReachableInTime.java
@@ -0,0 +1,9 @@
+package com.leetcode.medium;
+
+public class DetermineIfaCellIsReachableInTime {
+
+    public boolean isReachableAtTime(int sx, int sy, int fx, int fy, int t) {
+        int need = Math.max(Math.abs(sx - fx), Math.abs(sy - fy));
+        return need > 0 ? t >= need : t != 1;
+    }
+}
