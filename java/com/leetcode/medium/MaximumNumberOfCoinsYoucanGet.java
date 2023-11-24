c23e403eb1e70960e39775d10ff1939bdf03abcb
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2023-11-24 22:48:39 +0530

    Update MaximumNumberOfCoinsYoucanGet

--- java/com/leetcode/medium/MaximumNumberOfCoinsYoucanGet.java
+++ java/com/leetcode/medium/MaximumNumberOfCoinsYoucanGet.java
@@ -8,9 +8,10 @@ public class MaximumNumberOfCoinsYoucanGet {
         Arrays.sort(piles);
         int n = piles.length;
         int max = 0;
-        
-        for(int i = n/3; i < n-1; i+=2) max += piles[i];
-        
+
+        for (int i = n / 3; i < n - 1; i += 2)
+            max += piles[i];
+
         return max;
     }
 }
