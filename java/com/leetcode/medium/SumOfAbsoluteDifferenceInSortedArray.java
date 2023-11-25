f90eef4b94e0c9c767076ca9e004c182baa2ca85
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2023-11-25 22:46:15 +0530

    Add SumOfAbsoluteDifferenceInSortedArray

---
+++ java/com/leetcode/medium/SumOfAbsoluteDifferenceInSortedArray.java
@@ -0,0 +1,17 @@
+package com.leetcode.medium;
+
+public class SumOfAbsoluteDifferenceInSortedArray {
+
+    public int[] getSumAbsoluteDifferences(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+        int[] prefixSum = new int[n + 1];
+        for (int i = 0; i < n; ++i) {
+            prefixSum[i + 1] = prefixSum[i] + nums[i];
+        }
+        for (int i = 0; i < n; ++i) {
+            res[i] = i * nums[i] - prefixSum[i] + (prefixSum[n] - prefixSum[i] - (n - i) * nums[i]);
+        }
+        return res;
+    }
+}
