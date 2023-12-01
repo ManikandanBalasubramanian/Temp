b2684017be60144991657f46a5533da4ebd4a1c6
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2023-12-01 22:22:31 +0530

    update CheckIfTwoStringArraysAreEquivalent

--- java/com/leetcode/easy/CheckIfTwoStringArraysAreEquivalent.java
+++ java/com/leetcode/easy/CheckIfTwoStringArraysAreEquivalent.java
@@ -7,6 +7,7 @@ import java.util.Collectors;
 public class CheckIfTwoStringArraysAreEquivalent {
 
     Function<String[], String> joiner = (str) -> Arrays.stream(str).collect(Collectors.joining(""));
+
     public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
         return joiner.apply(word1).equals(joiner.apply(word2));
     }
