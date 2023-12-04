a4436b55a7cfe12cb24d3ecb73094df3e269b811
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2023-12-04 22:18:15 +0530

    Add Largest3SameDigitNumberInString

---
+++ java/com/leetcode/easy/Largest3SameDigitNumberInString.java
@@ -0,0 +1,13 @@
+package com.leetcode.easy;
+
+public class Largest3SameDigitNumberInString {
+
+    public String largestGoodInteger(String num) {
+        String ans = "";
+        for (int i = 2; i < num.length(); i++)
+            if (num.charAt(i) == num.charAt(i - 1) && num.charAt(i - 1) == num.charAt(i - 2))
+                if (num.substring(i - 2, i + 1).compareTo(ans) > 0)
+                    ans = num.substring(i - 2, i + 1);
+        return ans;
+    }
+}
