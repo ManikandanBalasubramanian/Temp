a282f148600366ae6aad8e36ed189ed9076dab52
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2023-12-08 22:10:07 +0530

    Update ConvertStringFromBinaryTree

--- java/com/leetcode/easy/ConvertStringFromBinaryTree.java
+++ java/com/leetcode/easy/ConvertStringFromBinaryTree.java
@@ -5,10 +5,14 @@ import com.leetcode.utils.TreeNode;
 public class ConvertStringFromBinaryTree {
 
     public String tree2str(TreeNode root) {
-        if (root == null) return "";
-        if(root.left == null && root.right == null) return root.val + "";
-        if(root.right == null) return root.val + "(" + tree2str(root.left) + ")";
 
-        return root.val + "(" + tree2str(root.left) + ")" +"(" + tree2str(root.right) + ")";
+        if (root == null)
+            return "";
+        if (root.left == null && root.right == null)
+            return root.val + "";
+        if (root.right == null)
+            return root.val + "(" + tree2str(root.left) + ")";
+
+        return root.val + "(" + tree2str(root.left) + ")" + "(" + tree2str(root.right) + ")";
     }
 }
