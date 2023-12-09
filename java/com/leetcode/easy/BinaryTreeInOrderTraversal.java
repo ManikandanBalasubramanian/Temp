8d51d940179600900d5fcdbba1ebd9333bb91710
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2023-12-09 22:08:41 +0530

    Update BinaryTreeInOrderTraversal

--- java/com/leetcode/easy/BinaryTreeInOrderTraversal.java
+++ java/com/leetcode/easy/BinaryTreeInOrderTraversal.java
@@ -7,9 +7,11 @@ import java.util.ArrayList;
 public class BinaryTreeInOrderTraversal {
 
     List<Integer> list = new ArrayList<>();
-    
+
     public List<Integer> inorderTraversal(TreeNode root) {
-        if(root == null) return list;
+
+        if (root == null)
+            return list;
         inorderTraversal(root.left);
         list.add(root.val);
         inorderTraversal(root.right);
