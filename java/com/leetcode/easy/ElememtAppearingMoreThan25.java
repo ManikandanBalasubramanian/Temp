d8479eb2086115e303a3f20d9035a224300dfe0c
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2023-12-11 15:04:53 +0530

    Add ElememtAppearingMoreThan25

---
+++ java/com/leetcode/easy/ElememtAppearingMoreThan25.java
@@ -0,0 +1,25 @@
+package com.leetcode.easy;
+
+public class ElememtAppearingMoreThan25 {
+
+    public int findSpecialInteger(int[] arr) {
+        int size = arr.length;
+        int qtr = size / 4;
+        int cnt = 1;
+        int p = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+
+            if (p == arr[i])
+                cnt++;
+            else
+                cnt = 1;
+
+            if (cnt > qtr)
+                return arr[i];
+
+            p = arr[i];
+        }
+
+        return p;
+    }
+}
