22a2370f93c310a1c075538e2f71a152584468b3
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2023-12-24 00:59:26 +0530

    Add MinimumChangesToMakeAlternatingBinaryString

---
+++ java/com/leetcode/easy/MinimumChangesToMakeAlternatingBinaryString.java
@@ -0,0 +1,17 @@
+package com.leetcode.easy;
+
+public class MinimumChangesToMakeAlternatingBinaryString {
+
+    public int minOperations(String s) {
+        int n = s.length(), a = 0, b = 0;
+        for (int i = 0; i < n; ++i) {
+            // if (s.charAt(i) - '0' == i % 2) {
+            if (s.charAt(i) == '0' + i % 2) {
+                ++a;
+            } else {
+                ++b;
+            }
+        }
+        return Math.min(a, b);
+    }
+}
