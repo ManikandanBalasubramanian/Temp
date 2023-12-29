8c9d215f9b28c5262acff1308e51a5eefe324d67
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2023-12-30 00:51:00 +0530

    Add RedistributeCharactersToMakeAllStringsEqual

---
+++ java/com/leetcode/easy/RedistributeCharactersToMakeAllStringsEqual.java
@@ -0,0 +1,24 @@
+package com.leetcode.easy;
+
+public class RedistributeCharactersToMakeAllStringsEqual {
+
+    public boolean makeEqual(String[] words) {
+        Map<Character, Integer> charCount = new HashMap<>();
+
+        for (String word : words) {
+            for (char c : word.toCharArray()) {
+                charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+            }
+        }
+
+        int n = words.length;
+
+        for (Map.Entry<Character, Integer> entry : charCount.entrySet()) {
+            if (entry.getValue() % n != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
