7ea8e0197294bfc28d44a61d53a3f32409e17de5
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2024-01-02 09:34:19 +0530

    Add ConvertArrayTo2DArrayWithConditions

---
+++ java/com/leetcode/medium/ConvertArrayTo2DArrayWithConditions.java
@@ -0,0 +1,23 @@
+package com.leetcode.medium;
+
+public class ConvertArrayTo2DArrayWithConditions {
+
+    public List<List<Integer>> findMatrix(int[] nums) {
+        int n = nums.length;
+        int[] freq = new int[n + 1];
+        int max = 0;
+        for (int num : nums) {
+            freq[num]++;
+            max = Math.max(max, freq[num]);
+        }
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < max; i++)
+            ans.add(new ArrayList<>());
+        for (int i = 0; i <= n; i++) {
+            while (freq[i]-- > 0) {
+                ans.get(freq[i]).add(i);
+            }
+        }
+        return ans;
+    }
+}
