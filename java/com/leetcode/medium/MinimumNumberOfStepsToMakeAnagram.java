02afd0cf11516df7f99fc9e43a0fe80d5530caeb
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2024-01-13 07:37:47 +0530

    Add MinimumNumberOfStepsToMakeAnagram

---
+++ java/com/leetcode/medium/MinimumNumberOfStepsToMakeAnagram.java
@@ -0,0 +1,20 @@
+package com.leetcode.medium;
+
+class MinimumNumberOfStepsToMakeAnagram {
+
+    public int minSteps(String s, String t) {
+        int[] count = new int[26];
+        for (int i = 0; i < s.length(); ++i) {
+            ++count[s.charAt(i) - 'a'];
+            --count[t.charAt(i) - 'a'];
+        }
+
+        int steps = 0;
+        for (int step : count) {
+            if (step > 0) {
+                steps += step;
+            }
+        }
+        return steps;
+    }
+}
