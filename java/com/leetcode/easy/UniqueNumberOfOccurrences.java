b36cd19f29e3a9c3fec1a5deb70f06a5b97fb636
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2024-01-17 12:33:33 +0530

    Update UniqueNumberOfOccurrences

--- java/com/leetcode/easy/UniqueNumberOfOccurrences.java
+++ java/com/leetcode/easy/UniqueNumberOfOccurrences.java
@@ -8,10 +8,12 @@ import java.util.HashSet;
 public class UniqueNumberOfOccurrences {
 
     public boolean uniqueOccurrences(int[] arr) {
+
         Map<Integer, Integer> count = new HashMap<>();
         for (int a : arr) {
             count.merge(a, 1, Integer::sum);
         }
+
         Set<Integer> seen = new HashSet<>();
         for (int freq : count.values()) {
             if (!seen.add(freq)) {
