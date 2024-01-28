cc4a3807d447e234780480e1ffaf8ee13b0908b8
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2024-01-28 21:28:12 +0530

    Update MaximumLengthOfConcatenatedString

--- java/com/leetcode/medium/MaximumLengthOfConcatenatedString.java
+++ java/com/leetcode/medium/MaximumLengthOfConcatenatedString.java
@@ -2,27 +2,30 @@ package com.leetcode.medium;
 
 public class MaximumLengthOfConcatenatedString {
 
-    int max =0;
+    int max = 0;
+
     public int maxLength(List<String> arr) {
-        backTrack(arr,"",0);
+        backTrack(arr, "", 0);
         return max;
     }
-    
-    private void backTrack(List<String> arr, String current, int start){
-        if(max<current.length())
-            max =current.length();
-        for(int i=start; i<arr.size();i++){
-            if(!isValid(current,arr.get(i))) continue;
-            backTrack(arr,current+arr.get(i),i+1);
+
+    private void backTrack(List<String> arr, String current, int start) {
+        if (max < current.length())
+            max = current.length();
+        for (int i = start; i < arr.size(); i++) {
+            if (!isValid(current, arr.get(i)))
+                continue;
+            backTrack(arr, current + arr.get(i), i + 1);
         }
     }
-    
-    private boolean isValid(String currentString, String newString){
+
+    private boolean isValid(String currentString, String newString) {
         int[] array = new int[26];
-        for(int i=0;i<newString.length();i++){
-            if(++array[newString.charAt(i)-'a'] == 2) return false;
-            
-            if(currentString.contains(newString.charAt(i)+""))
+        for (int i = 0; i < newString.length(); i++) {
+            if (++array[newString.charAt(i) - 'a'] == 2)
+                return false;
+
+            if (currentString.contains(newString.charAt(i) + ""))
                 return false;
         }
         return true;
