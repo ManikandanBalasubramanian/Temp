d280a27acdefe954bddd5a186eed9f92c0265356
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2024-01-30 10:10:22 +0530

    Update EvaluateReversePolishNotion

--- java/com/leetcode/medium/EvaluateReversePolishNotion.java
+++ java/com/leetcode/medium/EvaluateReversePolishNotion.java
@@ -7,11 +7,16 @@ public class EvaluateReversePolishNotion {
     public int evalRPN(String[] tokens) {
         Stack<Integer> stack = new Stack<>();
         for (String s : tokens) {
-            if ("+".equals(s)) stack.push(stack.pop() + stack.pop());
-            else if ("-".equals(s)) stack.push(-stack.pop() + stack.pop());
-            else if ("/".equals(s)) stack.push((int)(1D / stack.pop() * stack.pop()));
-            else if ("*".equals(s)) stack.push(stack.pop() * stack.pop());
-            else stack.push(Integer.valueOf(s));
+            if ("+".equals(s))
+                stack.push(stack.pop() + stack.pop());
+            else if ("-".equals(s))
+                stack.push(-stack.pop() + stack.pop());
+            else if ("/".equals(s))
+                stack.push((int) (1D / stack.pop() * stack.pop()));
+            else if ("*".equals(s))
+                stack.push(stack.pop() * stack.pop());
+            else
+                stack.push(Integer.valueOf(s));
         }
 
         return stack.pop();
