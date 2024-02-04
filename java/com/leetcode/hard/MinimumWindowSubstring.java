eabe5c82dc6d6231d5c1e48bb061192c6fe31af8
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2024-02-04 10:04:48 +0530

    Update MinimumWindowSubstring

--- java/com/leetcode/hard/MinimumWindowSubstring.java
+++ java/com/leetcode/hard/MinimumWindowSubstring.java
@@ -10,23 +10,25 @@ public class MinimumWindowSubstring {
         int begin = 0;
         int len = Integer.MAX_VALUE;
         int count = t.length();
-        for (int left=0, right=0; right<s.length(); right++) {
+        for (int left = 0, right = 0; right < s.length(); right++) {
             char c = s.charAt(right);
             map[c]--;
-            if(map[c]>=0) count--; 
+            if (map[c] >= 0)
+                count--;
             while (count == 0) {
                 char lc = s.charAt(left);
                 map[lc]++;
-                if (map[lc]>0) {
-                    if (right-left+1<len) {
+                if (map[lc] > 0) {
+                    if (right - left + 1 < len) {
                         begin = left;
-                        len = right-left+1;
+                        len = right - left + 1;
                     }
                     count++;
                 }
                 left++;
             }
         }
-        return len==Integer.MAX_VALUE?"":s.substring(begin, begin+len);
+        return len == Integer.MAX_VALUE ? "" : s.substring(begin, begin + len);
     }
+
 }
