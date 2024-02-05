90ece7bbb8b7c3485a3dc2a256744f9a6f2ba56a
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2024-02-05 08:26:19 +0530

    Update FirstUniqueCharacterInaString

--- java/com/leetcode/easy/FirstUniqueCharacterInaString.java
+++ java/com/leetcode/easy/FirstUniqueCharacterInaString.java
@@ -1,19 +1,19 @@
 package com.leetcode.easy;
 
-
 public class FirstUniqueCharacterInaString {
     public int firstUniqChar(String s) {
         int index = -1;
         int count[] = new int[26];
-        
-        for(char c : s.toCharArray()) {
-            count[c-'a']++;
+
+        for (char c : s.toCharArray()) {
+            count[c - 'a']++;
         }
-        
-        for(int i = 0; i < s.length(); i++) {
-            if(count[s.charAt(i)-'a'] == 1) return i;
+
+        for (int i = 0; i < s.length(); i++) {
+            if (count[s.charAt(i) - 'a'] == 1)
+                return i;
         }
-            
+
         return index;
     }
 }
