33f2e136db0dc65fffb466a872566111157010a0
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2024-02-06 08:25:18 +0530

    Update GroupAnagrams

--- java/com/leetcode/medium/GroupAnagrams.java
+++ java/com/leetcode/medium/GroupAnagrams.java
@@ -8,16 +8,18 @@ import java.util.HashMap;
 
 public class GroupAnagrams {
 
-	public List<List<String>> groupAnagrams(String[] strs) {
-        if (strs == null || strs.length == 0) return new ArrayList<>();
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if (strs == null || strs.length == 0)
+            return new ArrayList<>();
         Map<String, List<String>> map = new HashMap<>();
         for (String s : strs) {
             char[] ca = s.toCharArray();
             Arrays.sort(ca);
             String keyStr = String.valueOf(ca);
-            if (!map.containsKey(keyStr)) map.put(keyStr, new ArrayList<>());
+            if (!map.containsKey(keyStr))
+                map.put(keyStr, new ArrayList<>());
             map.get(keyStr).add(s);
         }
-        return new ArrayList<>(map.values());   
+        return new ArrayList<>(map.values());
     }
 }
