173977c70d1fdf5f2558e18ed83cbadd7ab92b69
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2024-02-07 08:24:12 +0530

    Update SortCharactersByFrequency

--- java/com/leetcode/medium/SortCharactersByFrequency.java
+++ java/com/leetcode/medium/SortCharactersByFrequency.java
@@ -10,16 +10,17 @@ public class SortCharactersByFrequency {
 
     public String frequencySort(String s) {
         Map<Character, Integer> map = new HashMap<>();
-        for (char c : s.toCharArray()) 
+        for (char c : s.toCharArray())
             map.put(c, map.getOrDefault(c, 0) + 1);
-						
-        List<Character> [] bucket = new List[s.length() + 1];
+
+        List<Character>[] bucket = new List[s.length() + 1];
         for (char key : map.keySet()) {
             int frequency = map.get(key);
-            if (bucket[frequency] == null) bucket[frequency] = new ArrayList<>();
+            if (bucket[frequency] == null)
+                bucket[frequency] = new ArrayList<>();
             bucket[frequency].add(key);
         }
-				
+
         StringBuilder sb = new StringBuilder();
         for (int pos = bucket.length - 1; pos >= 0; pos--)
             if (bucket[pos] != null)
