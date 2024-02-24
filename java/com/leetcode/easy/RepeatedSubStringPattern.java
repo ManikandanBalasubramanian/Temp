c62a0b30a40d5147b3583102582b78129a80682f
Manikandan Balasubramanian <manikandan.1304117@srec.ac.in>
2024-02-24 11:11:24 +0530

    Update

--- java/com/leetcode/easy/RepeatedSubStringPattern.java
+++ java/com/leetcode/easy/RepeatedSubStringPattern.java
@@ -1,6 +1,6 @@
 package com.leetcode.easy;
 
-public class RepeatedSubstringPattern {
+public class RepeatedSubStringPattern {
     public boolean repeatedSubstringPattern(String str) {
         String s = str + str;
         return s.substring(1, s.length() - 1).contains(str);
