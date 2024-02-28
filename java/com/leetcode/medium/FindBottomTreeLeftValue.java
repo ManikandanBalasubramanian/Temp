196343119c699a63810aec54a25917da338a46cb
ManikandanBalasubramanian <manikandan.1304117@srec.ac.in>
2024-02-28 13:35:17 +0530

    Add FindBottomTreeLeftValue

---
+++ java/com/leetcode/medium/FindBottomTreeLeftValue.java
@@ -0,0 +1,20 @@
+package com.leetcode.medium;
+
+public class FindBottomTreeLeftValue {
+
+    public int findBottomLeftValue(TreeNode root) {
+        return findBottomLeftValue(root, 1, new int[] { 0, 0 });
+    }
+
+    public int findBottomLeftValue(TreeNode root, int depth, int[] res) {
+        if (res[1] < depth) {
+            res[0] = root.val;
+            res[1] = depth;
+        }
+        if (root.left != null)
+            findBottomLeftValue(root.left, depth + 1, res);
+        if (root.right != null)
+            findBottomLeftValue(root.right, depth + 1, res);
+        return res[0];
+    }
+}
