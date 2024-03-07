961816b964203a9e40e8764ba32f0f6b3dcec9b0
Manikandan Balasubramanian <manikandan.1304117@srec.ac.in>
2024-03-08 00:27:31 +0530

    Update MiddleOftheLinkedList

--- java/com/leetcode/easy/MiddleOftheLinkedList.java
+++ java/com/leetcode/easy/MiddleOftheLinkedList.java
@@ -3,6 +3,7 @@ package com.leetcode.easy;
 import com.leetcode.utils.ListNode;
 
 public class MiddleOftheLinkedList {
+    
     public ListNode middleNode(ListNode tortoise) {
         ListNode hare = tortoise;
         
