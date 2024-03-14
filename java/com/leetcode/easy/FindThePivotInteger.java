ee3bae3974e9f0823d484c6371f80dde6f1aa347
Manikandan Balasubramanian <manikandan.1304117@srec.ac.in>
2024-03-15 00:18:53 +0530

    Update FindThePivotInteger

--- java/com/leetcode/easy/FindThePivotInteger.java
+++ java/com/leetcode/easy/FindThePivotInteger.java
@@ -3,6 +3,7 @@ package com.leetcode.easy;
 public class FindThePivotInteger {
 
     public int pivotInteger(int n) {
+        
         int ans = (n * n + n ) /2;
         int sq = (int)Math.sqrt(ans);
         if(sq * sq == ans)return sq;
