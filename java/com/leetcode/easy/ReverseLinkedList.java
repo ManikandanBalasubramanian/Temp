d15cdf608661d69dfe9f89e43c5171ed9e97692f
Manikandan Balasubramanian <manikandan.1304117@srec.ac.in>
2024-03-22 00:09:44 +0530

    update ReverseLinkedList

--- java/com/leetcode/easy/ReverseLinkedList.java
+++ java/com/leetcode/easy/ReverseLinkedList.java
@@ -3,7 +3,9 @@ package com.leetcode.easy;
 import com.leetcode.utils.ListNode;
 
 public class ReverseLinkedList {
-	public ListNode reverseList(ListNode head) {
+
+    public ListNode reverseList(ListNode head) {
+
         ListNode prev = null, next = null;
         
         while(head != null) {
