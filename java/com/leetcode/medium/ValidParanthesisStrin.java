bc47e0374c07a430b0f363aa692fd712acd8e23d
Manikandan Balasubramanian <manikandan.1304117@srec.ac.in>
2024-04-07 08:09:45 +0530

    Add ValidParanthesisStrin

---
+++ java/com/leetcode/medium/ValidParanthesisStrin.java
@@ -0,0 +1,21 @@
+package com.leetcode.medium;
+
+public class ValidParanthesisStrin {
+
+    public boolean checkValidString(String s) {
+        int leftCount = 0, rightCount = 0;
+        
+        for (char c : s.toCharArray()) {
+            leftCount += c == '(' ? 1 : -1;
+            rightCount += c == ')' ? -1 : 1;
+            
+            if (rightCount < 0) {
+                break;
+            }
+            
+            leftCount = Math.max(leftCount, 0);
+        }
+
+        return leftCount == 0;
+    }
+}
