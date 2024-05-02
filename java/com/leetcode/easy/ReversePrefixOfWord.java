9bd438d2066998780113206b9d78930333cb8878
Manikandan Balasubramanian <manikandan.1304117@srec.ac.in>
2024-05-02 11:04:45 +0530

    Add ReversePrefixOfWord

---
+++ java/com/leetcode/easy/ReversePrefixOfWord.java
@@ -0,0 +1,10 @@
+package com.leetcode.easy;
+
+class ReversePrefixOfWord {
+    
+    public String reversePrefix(String word, char ch) {
+        int idx = word.indexOf(ch)+1;
+		StringBuffer rev = new StringBuffer(word.substring(0, idx));
+		return rev.reverse()+word.substring(idx);
+    }
+}
