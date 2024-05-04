841ee615fcae85eef5cbb76d791c51fd4f06712a
Manikandan Balasubramanian <manikandan.1304117@srec.ac.in>
2024-05-04 21:17:03 +0530

    Update BoatsToSavePeople

--- java/com/leetcode/medium/BoatsToSavePeople.java
+++ java/com/leetcode/medium/BoatsToSavePeople.java
@@ -3,6 +3,7 @@ package com.leetcode.medium;
 public class BoatsToSavePeople {
 
     public int numRescueBoats(int[] people, int limit) {
+        
         Arrays.sort(people);
         int i = 0, j = people.length - 1;
         int boats = 0;
