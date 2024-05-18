719f6fe4539d079a5b23dd282fb594f5f4295af0
Manikandan Balasubramanian <manikandan.1304117@srec.ac.in>
2024-05-18 11:55:29 +0530

    Update EvaluateBooleanBinaryTree

--- java/com/leetcode/easy/EvaluateBooleanBinaryTree.java
+++ java/com/leetcode/easy/EvaluateBooleanBinaryTree.java
@@ -3,6 +3,7 @@ package com.leetcode.easy;
 public class EvaluateBooleanBinaryTree {
 
     public boolean evaluateTree(TreeNode root) {
+        
         if(root.val==1) return true;
         else if(root.val==0) return false;
         else if(root.val==2) return (evaluateTree(root.left)||evaluateTree(root.right));
