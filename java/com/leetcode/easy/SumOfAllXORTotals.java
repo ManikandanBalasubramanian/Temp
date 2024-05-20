8e9b3125d773f5cb8bb95b41da45023b3893d748
Manikandan Balasubramanian <manikandan.1304117@srec.ac.in>
2024-05-20 11:53:10 +0530

    Add SumOfAllXORTotals

---
+++ java/com/leetcode/easy/SumOfAllXORTotals.java
@@ -0,0 +1,19 @@
+package com.leetcode.easy;
+
+public class SumOfAllXORTotals {
+    
+    public int subsetXORSum(int[] nums) {
+        return findSum(nums, 0, 0);
+    }
+
+    private int findSum(int[] nums, int curInd, int curNum) {
+        if (curInd == nums.length) {
+            return curNum;
+        }
+
+        int includeInXor = findSum(nums, curInd + 1, curNum ^ nums[curInd]);
+        int notIncludeInXor = findSum(nums, curInd + 1, curNum);
+
+        return includeInXor + notIncludeInXor;
+    }
+}
