0b6eae9c132ceed4d2c2811806d739e6e0ce9524
Manikandan Balasubramanian <manikandan.1304117@srec.ac.in>
2024-06-04 11:31:01 +0530

    Add ScoreOfString

---
+++ java/com/leetcode/easy/ScoreOfString.java
@@ -0,0 +1,12 @@
+package com.leetcode.easy;
+
+public class ScoreOfString {
+    
+    public int scoreOfString(String s) {
+        int total = 0;
+        for(int i = 0; i < s.length() - 1; i++) {
+            total += Math.abs(s.charAt(i) - s.charAt(i + 1));
+        }
+        return total;
+    }
+}
