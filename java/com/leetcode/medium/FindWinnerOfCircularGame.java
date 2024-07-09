b51af0af96da95ed89e01a7564af78aa5e807264
Manikandan Balasubramanian <manikandan.1304117@srec.ac.in>
2024-07-09 23:12:25 +0530

    Add FindWinnerOfCircularGame

---
+++ java/com/leetcode/medium/FindWinnerOfCircularGame.java
@@ -0,0 +1,12 @@
+package com.leetcode.medium;
+
+public class FindWinnerOfCircularGame {
+    
+    public int findTheWinner(int n, int k) {
+        int winner = 1;
+        for(int groupSize = 2; groupSize <= n; groupSize++){
+            winner = ((winner + k - 1) % groupSize) + 1;
+        }
+        return winner;
+    }
+}
