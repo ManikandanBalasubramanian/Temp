fe2b80562df8603b71437aac5bb362d604329221
Manikandan Balasubramanian <manikandan.1304117@srec.ac.in>
2024-07-23 20:59:42 +0530

    Update SortAnArrayByIncreasingFrequency

--- java/com/leetcode/easy/SortAnArrayByIncreasingFrequency.java
+++ java/com/leetcode/easy/SortAnArrayByIncreasingFrequency.java
@@ -3,6 +3,7 @@ package com.leetcode.easy;
 public class SortAnArrayByIncreasingFrequency {
     
     public int[] frequencySort(int[] nums) {
+        
         Map<Integer, Integer> map=new HashMap<Integer, Integer>();
           for(int i=0;i<nums.length;i++){
             if(map.containsKey(nums[i])){
