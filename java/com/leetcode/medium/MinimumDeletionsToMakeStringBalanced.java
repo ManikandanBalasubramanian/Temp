3f5b93a48629b58bc86fbb81d95e7cdca0019316
Manikandan Balasubramanian <manikandan.1304117@srec.ac.in>
2024-07-30 20:49:30 +0530

    Add MinimumDeletionsToMakeStringBalanced

---
+++ java/com/leetcode/medium/MinimumDeletionsToMakeStringBalanced.java
@@ -0,0 +1,16 @@
+package com.leetcode.medium;
+
+public class MinimumDeletionsToMakeStringBalanced {
+    
+    public int minimumDeletions(String s) {
+        int ans = 0, countOfB = 0;
+        for(char c : s.toCharArray()) {
+            if(c == 'a') {
+                ans = Math.min(ans + 1, countOfB);
+            } else {
+                countOfB++;
+            }
+        }
+        return ans;
+    }
+}
