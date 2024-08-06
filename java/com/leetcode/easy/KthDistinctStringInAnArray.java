9d780608e918ab86b0ddc21357279675d701abdc
Manikandan Balasubramanian <manikandan.1304117@srec.ac.in>
2024-08-06 20:23:36 +0530

    Add KthDistinctStringInAnArray

---
+++ java/com/leetcode/easy/KthDistinctStringInAnArray.java
@@ -0,0 +1,19 @@
+package com.leetcode.easy;
+
+public class KthDistinctStringInAnArray {
+    
+    public String kthDistinct(String[] arr, int k) {
+        HashMap<String, Integer> map = new HashMap<>();
+        for (String i : arr) {
+            map.put(i, map.getOrDefault(i, 0) + 1);
+        }
+
+        int count = 0;
+        for (String s : arr) {
+            if (map.get(s) == 1 && ++count == k) {
+                return s;
+            }
+        }
+        return "";
+    }
+}
