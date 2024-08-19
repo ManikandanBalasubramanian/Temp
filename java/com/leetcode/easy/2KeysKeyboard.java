66ee8ed2f423b21e1856ef7056c5e409ea3d80f6
Manikandan Balasubramanian <manikandan.1304117@srec.ac.in>
2024-08-19 19:53:08 +0530

    Add 2KeysKeyboard

---
+++ java/com/leetcode/easy/2KeysKeyboard.java
@@ -0,0 +1,22 @@
+package com.leetcode.easy;
+
+class 2KeysKeyboard {
+    
+    public int minSteps(int n) {
+        if (n==1){
+            return 0;
+        }
+        int o=0;
+        int p=1;
+        int c=0;
+        while(p!=n){
+            if(n%p==0){
+                c=p;
+                o++;
+            }
+            p+=c;
+            o++;
+        }
+        return o;
+    }
+}
