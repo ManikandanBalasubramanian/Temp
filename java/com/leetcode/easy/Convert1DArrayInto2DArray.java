813c1808d74bd5bad31b950aaabecf3db454e775
Manikandan Balasubramanian <manikandan.1304117@srec.ac.in>
2024-09-03 14:46:25 +0530

    Add Convert1DArrayInto2DArray

---
+++ java/com/leetcode/easy/Convert1DArrayInto2DArray.java
@@ -0,0 +1,16 @@
+package com.leetcode.easy;
+
+public class Convert1DArrayInto2DArray {
+    public int[][] construct2DArray(int[] original, int m, int n) {
+        if (m * n != original.length) return new int[0][0];
+
+        int [][] res = new int[m][n];
+        int idx = 0;
+
+        for (int i = 0 ; i < m ; i++) {
+            for (int j = 0 ; j < n ; j++)
+                res[i][j] = original[idx++];
+        }
+        return res;
+    }
+}
